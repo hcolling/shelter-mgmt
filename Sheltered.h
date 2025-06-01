9ce9f6fcd62173c9c31f62fa7370627111d6ccbe
Tobias Klein <152293349+TobiasOKlein@users.noreply.github.com>
2025-06-01 17:49:51 -0300

    Add files via upload

---
+++ Sheltered.h
@@ -0,0 +1,46 @@
+#ifndef SHELTERED_H
+#define SHELTERED_H
+#include "Person.h"
+#include <string>
+
+using namespace std;
+
+class Sheltered : public Person {
+public:
+    Sheltered();
+    ~Sheltered();
+    Sheltered(std::string name, int birthDate[DATES], int gender, std::string cpf, std::string nationality, int bloodType);
+    std::string getResponsible();
+    
+
+    void requestResources();
+    bool isNeedingResources();
+    void setNeedResources(bool value);
+    void setResponsible(std::string r);
+
+    /* AINDA NAO DESENVOLVIDOS */
+    //void requestHealthAssist();
+    //bool isNeedingHealthAssist();
+    //void setNeedHealthAssist(bool value);
+
+private:
+    std::string responsible;
+    bool needResources;
+    bool needHealthAssist;
+};
+Sheltered::Sheltered(){}
+
+Sheltered::~Sheltered(){}
+Sheltered::Sheltered(std::string name, int birthDate[DATES], int gender, std::string cpf, std::string nationality, int bloodType):Person(name, birthDate, gender, cpf, nationality, bloodType){}
+
+std::string Sheltered::getResponsible() { return responsible; }
+void Sheltered::setResponsible(std::string r) {
+    if (getAge() < 18) responsible = r;
+    else responsible = "N/A";
+}
+
+void Sheltered::requestResources() { needResources = true; }
+bool Sheltered::isNeedingResources(){ return needResources; }
+void Sheltered::setNeedResources(bool value) { needResources = value; }
+
+#endif // SHELTERED_H
